67169b81920b1ee751a80ab4c1cd90d01cfc4283
Juho Härme <juho.harme@uta.fi>
2018-01-04 13:01:47 +0200

    lisää edeltävä kappale dataan r-paketin puolelle

---
+++ rpackage/R/data.R
@@ -0,0 +1,11 @@
+#' Ensimmäistä asumista käsittelevää kappaletta edeltävä kappale ja tekstin id
+#'
+#' Tällä vain täydennetään jo olemassaolevaa dataa
+#'
+#' @format A data frame with the following structure:
+#' \describe{
+#'   \item{textid}{Tekstin id}
+#'   \item{previous_paragraph}{Edellisen kappaleen teksti}
+#'   ...
+#' }
+"previous_pars"
