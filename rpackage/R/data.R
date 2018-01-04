f32677e465c19bf1c841508a581dbd4b21c8e9c3
Juho Härme <juho.harme@uta.fi>
2018-01-04 13:34:20 +0200

    ja sitten vielä lisätty mahdollinen väliotsikko r-pakettiin

--- rpackage/R/data.R
+++ rpackage/R/data.R
@@ -9,3 +9,15 @@
 #'   ...
 #' }
 "previous_pars"
+
+#' Ensimmäistä asumista käsittelevää kappaletta edeltävä välitosikko ja tekstin id
+#'
+#' Tällä vain täydennetään jo olemassaolevaa dataa
+#'
+#' @format A data frame with the following structure:
+#' \describe{
+#'   \item{textid}{Tekstin id}
+#'   \item{chaptertitle}{Edellisen kappaleen väliotsikko}
+#'   ...
+#' }
+"chaptertitles"
