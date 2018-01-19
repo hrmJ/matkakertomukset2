0a2a398339029baebfba82f459f858b9174f1461
Juho Härme <juho.harme@gmail.com>
2018-01-19 09:25:11 +0200

    Luokittelupuu-pdf

--- data/visualize_groups_as_inference_tree.R
+++ data/visualize_groups_as_inference_tree.R
@@ -1,12 +1,17 @@
 
 library(SataTapaa)
 library(visNetwork)
+library(rpart.plot)
 
 set.seed(204)
 library(rpart)
-#puu <- ctree(group ~ side + location + dep + pers,stats)
-stats <- FormatForStatisticalAnalysis()
+stats <- FormatForStatisticalAnalysis(T)
 puu <- rpart(group ~ side + location + dep + pers + pos,stats)
+rpart.plot(puu,box.palette=0,extra=100,fallen.leaves=T,type=4)
+
 htmltree <- visTree(puu)
 htmltree
 visSave(htmltree,file="visualisaatio_puu.html")
+
+library(party)
+plot(ctree(group ~ side + location + dep + pers + pos,stats))
